97d40dcf81e276e2ea5ba5fed189634aee039164
aliergcr <46872079+aliergcr@users.noreply.github.com>
2019-10-10 20:00:05 +0300

    ...

--- src/components/NewsView.js
+++ src/components/NewsView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SafeAreaView,  TouchableHighlight } from 'react-native';
+import { SafeAreaView,  TouchableHighlight, Linking } from 'react-native';
 import {Icon} from 'native-base'
 import HeaderComponent from './common/HeaderComponent'
 import WebViewComp from './common/WebViewComp'
@@ -22,20 +22,7 @@ export default class NewsView extends Component {
     console.log(url)
     return (
       <SafeAreaView>
-        {/* <HeaderComponent navigation={this.props.navigation} name='arrow-round-back' /> */}
-        <WebView
-                    source={{ uri: 'https://facebook.github.io/react-native/' }}
-                    //style={{ flex: 1 }}
-                // startInLoadingState={true}
-                // renderLoading={()=>{
-                //     return (
-                //         <View>
-                //           <ActivityIndicator style={{ flex: 1 }}
-                //             size='large'
-                //           />
-                //         </View>
-                //     )}}
-                />
+        {Linking.openURL(url)}
       </SafeAreaView>
     );
   }
